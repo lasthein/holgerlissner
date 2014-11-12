9bcb23270aea66cac5100ae097289e1cb09b375e
Rasmus Lasthein <lasthein@gmail.com>
2014-11-12 21:35:42 +0100

    Nightly commit on lasthein Digital Ocean droplet

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
 cd /var/www/holgerlissner
-git add -A > /dev/null
-git commit -am "Nightly commit on lasthein Digital Ocean droplet" > /dev/null
-git push origin online > /dev/null
+git add -A > /dev/null 2>&1
+git commit -am "Nightly commit on lasthein Digital Ocean droplet" > /dev/null 2>&1
+git push origin online > /dev/null 2>&1
 
 
 
